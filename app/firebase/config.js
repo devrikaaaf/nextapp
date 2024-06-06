da57e89813b31e4d748273b5c9fbe2cfe641f9d6
Devrika <devrikaft@gmail.com>
2024-06-06 13:31:30 +0700

    5th

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -19,10 +19,10 @@ const firebaseConfig = {
 
 
 // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 
 export { app, auth }
